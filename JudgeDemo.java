a77ed1e1dfb9814fe33bdafeb634493493a876be
matthewgraca <mgraca@cpp.edu>
2020-01-03 17:43:14 -0800

    added more test cases

--- JudgeDemo.java
+++ JudgeDemo.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
 public class JudgeDemo
 {
-  public static void main(String[] args)
+  public static void main(String[] args) throws IOException
   {
+    ArrayList<Team> tournamentRoster = new ArrayList<Team>();
     //  Team test
     //  testing constructors and toString
     System.out.println("Testing Team constructors and toString");
@@ -10,10 +13,27 @@ public class JudgeDemo
     System.out.println(teamTest);
     teamTest = new Team("CX", "Gab", "Matt Graca", "Richard Ding");
     System.out.println(teamTest);
+    System.out.println();
 
     //  Judge test
     //  testing constructors
     System.out.println("Testing Judge constructors and toString");
     Judge judgeTest = new Judge("Gab", "Jonathan Hu");
+    System.out.println(judgeTest);
+    System.out.println();
+
+    //  filling the tournament roster
+    System.out.println("Grabbing the PF tournament roster from a file, stuffing it into an array list, and outputting it.");
+    File myFile = new File("PF_Roster.txt");
+    Scanner inputFile = new Scanner(myFile);
+    while (inputFile.hasNext())
+    {
+      tournamentRoster.add(new Team("PF", inputFile.next(), inputFile.next(), inputFile.next()));
+    }
+    inputFile.close();
+    for (int i = 0; i < tournamentRoster.size(); ++i)
+    {
+      System.out.println(tournamentRoster.get(i));
+    }
   }
 }
